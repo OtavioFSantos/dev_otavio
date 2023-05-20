d2e6f1bbb1973537f5faa4d3572753040657a8df
otavio <otaviofs01@gmail.com>
2023-05-20 18:58:11 -0300

    added header to tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from "framer-motion";
+import { textVariant, fadeIn } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech) => (
-        <div className="w-28 h-28" key={tech.name}>
-          <BallCanvas icon={tech.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Techs</h2>
+        <p className={styles.sectionSubText}>what I've already played with</p>
+      </motion.div>
+
+      <div className="mt-12 flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((tech) => (
+          <div className="w-28 h-28" key={tech.name}>
+            <BallCanvas icon={tech.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
